f4da68abe5d83beb1911631dff76bcef510bb4bb
unknown <diegoamezaga@gmail.com>
2022-05-15 22:31:33 -0300

    inicio de tasklist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonTaskComponent } from './components/boton-task/boton-task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonTaskComponent
+    BotonTaskComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
