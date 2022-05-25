5c74f6f48aa9f90484a06d556d76540f376cb650
unknown <diegoamezaga@gmail.com>
2022-05-25 20:11:44 -0300

    formulario y boton submit con funcion add task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +9,8 @@ import { BotonTaskComponent } from './components/boton-task/boton-task.component
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
 
 @NgModule({
   declarations: [
@@ -15,13 +18,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     BotonTaskComponent,
     TasksComponent,
-    TasksItemComponent
+    TasksItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import {TASKS} from '../mock-tasks'
 import {Task} from '../Task'
 import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +26,13 @@ export class TaskService {
   deleteTask(task:Task): Observable<Task>{
     const url = `${this.apiUrl}/${task.id}`
     return this.http.delete<Task>(url)
+  }
+  updateTaskReminder(task:Task){
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task:Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
   }
 }
