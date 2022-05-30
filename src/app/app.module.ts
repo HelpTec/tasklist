3810fb631cf36a86c42b106cff5a042f28bc80fa
unknown <diegoamezaga@gmail.com>
2022-05-29 21:11:28 -0300

    about con rutas agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonTaskComponent } from './components/boton-task/boton-task.component';
@@ -10,7 +10,13 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes} from '@angular/router';
 
+const appRouter: Routes=[
+  {path:'', component: TasksComponent},
+  {path:'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,14 +25,16 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     BotonTaskComponent,
     TasksComponent,
     TasksItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRouter, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
